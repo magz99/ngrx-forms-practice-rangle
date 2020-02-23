612079fd0a39fa25d883b6c5b8c28da0f916a3da
Magali Bautista-Lee <magzb.99@gmail.com>
2020-02-22 23:08:51 -0500

    Fixing validation for latest ngrx

--- src/app/store/app.reducer.ts
+++ src/app/store/app.reducer.ts
@@ -2,7 +2,12 @@ import {
   createFormStateReducerWithUpdate,
   updateGroup,
   validate,
-  createFormGroupState
+  createFormGroupState,
+  AbstractControlState,
+  FormGroupState,
+  disable,
+  FormGroupControls,
+  FormControlState
 } from 'ngrx-forms';
 import { required, greaterThanOrEqualTo } from 'ngrx-forms/validation';
 import { Config, Person } from '../models/example.model';
@@ -25,22 +30,41 @@ export const initialState: AppState = {
 
 // The `updateGroup` function allows us to define person and config as
 // nested forms (so that they can be displayed independently)
-const validateAndUpdateFormState = updateGroup<RootForm>({
-  person: updateGroup<Person>({
-    firstName: validate([required]),
-    lastName: validate([required])
-  }),
-  config: updateGroup<Config>({
-    minAge: validate([required, greaterThanOrEqualTo(0)])
-  })
-});
+const validateAndUpdateFormState = updateGroup<RootForm>([
+  {
+    person: updateGroup<Person>({
+      firstName: validate([required]),
+      lastName: validate([required])
+    }),
+    config: updateGroup<Config>({
+      minAge: validate([required, greaterThanOrEqualTo(0)])
+    })
+  },
+  {
+    person: (person, rootForm) =>
+      updateGroup<Person>({
+        age: age => {
+          const minAgeValue = rootForm.controls.config.controls.minAge.value;
+          return validate<number>(age, minAge(minAgeValue));
+        }
+      })(person)
+  }
+]);
 
 // createFormStateReducerWithUpdate defines validation rules of our form
 // We get back a reducer function that we can use in our rootReducer.
-export const formReducer = createFormStateReducerWithUpdate<RootForm>(
+export const formReducer = createFormStateReducerWithUpdate<RootForm>([
   validateAndUpdateFormState
-);
+]);
 
 export const rootReducer = {
   form: formReducer
 };
+
+// HELPERS
+
+export function minAge(minimumAge: number) {
+  return (value: number) => {
+    return value >= minimumAge ? null : { minAge: true };
+  };
+}
